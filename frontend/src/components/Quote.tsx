d365bcdfcc4e61f3a4b085a27ec94747e9e5d7cd
RudraNPradhan <rudrapradhan0916p@gmail.com>
2024-05-10 12:46:22 +0530

    Backend Only

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,12 @@
+export const Quote = () => {
+    return <div className="bg-slate-200 h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+            <div className="max-w-md text-center text-3xl font-bold">
+                "The customer support I recieved was exceptional. The support team went above and beyond to address my concerns"
+            </div>
+        </div>
+        <div className="max-w-lg text-center font-semibold">
+                Julies Winfield
+        </div>
+    </div>
+}
